70ba6c8975e38579b1c79acf5ca46c0c3703bd78
Favour Akomolafe <akomolafefavour@gmail.comm>
2021-12-25 14:01:18 +0100

    commit

--- src/components/ProductComponent.js
+++ src/components/ProductComponent.js
@@ -2,18 +2,30 @@ import { useSelector } from "react-redux"
 
 const ProductComponent = () => {
     const products = useSelector(state => state.allProducts.products)
-    const {id, title} = products
-    return (
-        <div className="four coloumn wide">
+    const renderList = products.map((product) => {
+        const { id, title, image, price, category } = product
+        return (
+            <div className="four coloumn wide" key={id}>
             <div className="ui link cards">
                 <div className="card">
-                    <div className="image"></div>
+                    <div className="image">
+                        <img src={image} alt={title} />
+                    </div>
                     <div className="content">
                         <div className="header">{title}</div>
+                        <div className="meta price">$ {price}</div>
+                        <div className="meta">{category}</div>
                     </div>
                 </div>
             </div>
         </div>
+        )
+    })
+
+    return (
+        <>
+          {renderList}  
+        </>
     )
 }
 
